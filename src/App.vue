2bf89c81ede7857c31bedd1c1c824ebb0507d96e
Lukk133 <zielinskil@outlook.com>
2023-10-13 13:23:03 +0200

    pierwszy navbar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <Navbar></Navbar>
 
   <TodoApp></TodoApp>
 
@@ -6,13 +7,15 @@
 
 <script>
 import TodoApp from './components/TodoApp.vue'
+import Navbar from './components/Navbar.vue';
 
 
 
 export default {
   name: 'App',
   components: {
-    TodoApp
+    TodoApp,
+    Navbar
   }
 }
 </script>
